6fdcc5a03316f53e8c2e099c50bfa8396b548d69
Mohammed Tauseef <tauseefk@Mohammeds-MacBook-Pro.local>
2016-12-20 12:37:42 -0500

    initial commit

---
+++ app/filteredStems.js
@@ -0,0 +1,35 @@
+'use strict';
+const natural = require('natural'),
+  wordTokenizer = new natural.WordTokenizer(),
+  stemmer = natural.PorterStemmer,
+  stem = stemmer.stem;
+
+const map = require('ramda').map,
+  curry = require('ramda').curry,
+  filter = require('ramda').filter;
+
+
+const compose = function(f, g) {
+  return function(x) {
+    return f(g(x));
+  }
+}
+
+const c_tokenize = function(tkr) {
+  return function(str) {
+    return tkr.tokenize(str);
+  }
+}
+const trace = function(x) {
+  console.log(x);
+  return x;
+}
+const gtTwo = function(x) {
+  return x.length > 2;
+}
+const wordTkr = c_tokenize(wordTokenizer),
+  findStems = compose(map(stem), wordTkr),
+  filteredStems = compose(filter(gtTwo), findStems),
+  logFilteredStems = compose(trace, filteredStems);
+
+module.exports = filteredStems;
---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+const express = require('express'),
+  app = express(),
+  filteredStems = require('./app/filteredStems'),
+  myString = "I'm surprised! I didn't know you could make it.";
+
+app.set('port', (process.env.PORT || 5000));
+app.use('/app/', express.static(__dirname + '/app/'));
+
+app.get('/', function(req, res) {
+  res.send(filteredStems(req.query = myString));
+})
+
+app.listen(app.get('port'), function() {
+  console.log(`Server started at ${app.get('port')}`);
+})
