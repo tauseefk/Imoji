854d494e483815e3d0f5514290c5a0d75229fa8b
Mohammed Tauseef <tauseefk@github.com>
2016-12-28 12:42:54 -0500

    fetching images based on stems

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 const express = require('express'),
   app = express(),
   filteredStems = require('./lib/filteredStems'),
-  myString = "I'm surprised! I didn't know you could make it.";
+  flickr = require('./lib/flickr'),
+  defString = "I'm surprised! I didn't know you could make it.";
 
 app.set('port', (process.env.PORT || 5000));
 app.use('/lib/', express.static(__dirname + '/app/'));
-
 app.get('/', function(req, res) {
-  res.send(filteredStems(req.query = myString));
+  if(req.query.msg == undefined) {
+    req.query.msg = defString;
+  }
+  let stems = filteredStems(req.query.msg);
+  flickr.app(stems[0]);
+  res.send(flickr.data);
 })
 
 app.listen(app.get('port'), function() {
