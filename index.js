b2c7a13c955aad8bb04690c7b91438331a0ad278
Mohammed Tauseef <tauseefk@github.com>
2016-12-29 00:09:24 -0500

    getting only the first picture based on a tag and appending

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var flickr = require('./lib/flickr');
 var fStems = require('./lib/filteredStems');
 var _ = require('ramda');
-const defString = "cat dragged the mouse into the cage!";
+const defString = "Quick brown fox jumped over a lazy dog";
 var head = function(arr) {
   return arr[0];
 }
-var convertStemToImage = _.compose(flickr, head, fStems);
+var convertStemToImage = _.compose(_.map(flickr), fStems);
 convertStemToImage(defString);
