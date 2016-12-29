808ac4146f34652de909d7339a90a84e67675da9
Mohammed Tauseef <tauseefk@github.com>
2016-12-28 23:57:55 -0500

    var

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var flickr = require('./lib/flickr');
 var fStems = require('./lib/filteredStems');
 var _ = require('ramda');
 const defString = "cat dragged the mouse into the cage!";
-let head = function(arr) {
+var head = function(arr) {
   return arr[0];
 }
-let convertStemToImage = _.compose(flickr, head, fStems);
+var convertStemToImage = _.compose(flickr, head, fStems);
 convertStemToImage(defString);
