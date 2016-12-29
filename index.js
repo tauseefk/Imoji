4a5de9f0866f3199886380fd575e90ca96d79959
Mohammed Tauseef <tauseefk@github.com>
2016-12-28 23:12:31 -0500

    babel

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var flickr = require('./lib/flickr');
 var fStems = require('./lib/filteredStems');
 var _ = require('ramda');
-const defString = "I'm surprised! I didn't know you could make it.";
-let convertStemToImage = _.compose(_.map(flickr), fStems);
+const defString = "cat dragged the mouse into the cage!";
+let head = function(arr) {
+  return arr[0];
+}
+let convertStemToImage = _.compose(flickr, head, fStems);
 convertStemToImage(defString);
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,17 @@ module.exports = {
     nodemailer: "empty",
     net: "empty",
     child_process: "empty"
-  }
+  },
+  module: {
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }
+  ]
+}
 }
