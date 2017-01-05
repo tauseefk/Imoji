5507ed740ce9b8fddb4a46988e9b607e8328c718
Mohammed Tauseef <tauseefk@github.com>
2017-01-05 18:25:53 -0500

    input box

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var flickr = require('./lib/flickr');
 var fStems = require('./lib/filteredStems');
 var _ = require('ramda');
 const defString = "Quick brown fox jumped over a lazy dog";
+var appContainer = document.querySelector('.app');
 var head = function(arr) {
   return arr[0];
 }
 var convertStemToImage = _.compose(_.map(flickr), fStems);
 convertStemToImage(defString);
+
+document.querySelector(".input").addEventListener('change', function(e){
+  while(appContainer.lastChild) {
+    appContainer.removeChild(appContainer.lastChild);
+  }
+  convertStemToImage(e.target.value.trim());
+})
