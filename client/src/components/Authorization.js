f280948cf4e126c1c7bd088f3779e3216a4580fa
Tauseef Khan <tauseef25@gmail.com>
2018-03-13 10:38:43 -0700

    Update Authorization.js

--- client/src/components/Authorization.js
+++ client/src/components/Authorization.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Authorization = () => {
   return (
     <div>
-      <a className="u-outlineNone u-borderNone btn btn-primary background-accent font-N7 text-uppercase" href='/authorizeUser'>Authorize</a>
+      <a className="u-outlineNone u-borderNone btn btn-primary background-accent font-N7 text-uppercase font--sans" href='/authorizeUser'>Authorize</a>
     </div>
   )
 }
